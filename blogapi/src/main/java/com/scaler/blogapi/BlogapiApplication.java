51ecc9b48555e48c16362bcec9af8a335e6b862f
makhd00m <smhayat225@outlook.com>
2023-05-09 17:48:00 +0530

    blog API basic setup via basic Spring wizard
    Signed-off-by: makhd00m <smhayat225@outlook.com>

--- blogapi/src/main/java/com/scaler/blogapi/BlogapiApplication.java
+++ blogapi/src/main/java/com/scaler/blogapi/BlogapiApplication.java
@@ -1,4 +1,4 @@
-package com.example.blogapi;
+package com.scaler.blogapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blogapi/src/test/java/com/scaler/blogapi/BlogapiApplicationTests.java
+++ blogapi/src/test/java/com/scaler/blogapi/BlogapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.blogapi;
+package com.scaler.blogapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
